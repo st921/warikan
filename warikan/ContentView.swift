ec35dec061cdf39c32aa7496aa75a6c20e327867
st921 <syou.tana0921@icloud.com>
2026-02-12 23:26:07 +0900

    Initial Commit

---
+++ warikan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  warikan
+//
+//  Created by しょう on 2026/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ warikan/warikanApp.swift
@@ -0,0 +1,17 @@
+//
+//  warikanApp.swift
+//  warikan
+//
+//  Created by しょう on 2026/02/12.
+//
+
+import SwiftUI
+
+@main
+struct warikanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
